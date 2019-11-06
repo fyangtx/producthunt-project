426a9391886f2e20be138e9b69a2611421d4875f
frank yang <fyangtx@gmail.com>
2019-11-06 17:43:25 -0600

    signup init commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        # user entered info and wants to an account setup.
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                # check existing user name.               
+                user = User.objects.get(username = request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'Username already been taken!!!'})            
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')        
+            
+    else:
+        # user wants to enter info.
+        return render(request, 'accounts/signup.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
